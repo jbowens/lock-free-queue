80f854fe5ca173826dde2d197b81b262aef3a585
Jackson Owens <jackson_owens@brown.edu>
2013-11-15 14:25:27 -0500

    lolz, no header file definitions plz

--- queue.c
+++ queue.c
@@ -1,1 +1,9 @@
 #include "queue.h"
+
+void lockfree_queue_init(lockfree_queue_t *queue)
+{
+    /* Setup the sentinel nodes. */
+    queue->q_head = &queue->_sentinel_head;
+    queue->_sentinel_head.n_next = &queue->_sentinel_tail;
+    queue->_sentinel_tail.n_next = 0;
+}
--- queue.h
+++ queue.h
@@ -32,11 +32,4 @@ extern void(*qnode_deallocator)(lockfree_qnode_t *);
 /**
  * Initializes a new lockfree queue.
  */
-void lockfree_queue_init(lockfree_queue_t *queue)
-{
-    /* Setup the sentinel nodes. */
-    queue->q_head = &queue->_sentinel_head;
-    queue->_sentinel_head.n_next = &queue->_sentinel_tail;
-    queue->_sentinel_tail.n_next = 0;
-}
-
+void lockfree_queue_init(lockfree_queue_t *queue);
