560bdfeba494e642e1d2ea6c6350ac0e64726564
Jackson Owens <jackson_owens@brown.edu>
2013-11-16 08:09:30 -0500

    Updated atomic.h comments

--- atomic.h
+++ atomic.h
@@ -1,7 +1,6 @@
 #pragma once
 
 /**
- * Compare and set. Intended only for 32 bit machines. Returns 1 if successful
- * 0 otherwise.
+ * Compare and set macro.
  */
 #define compare_and_set(loc, old_value, new_value) __sync_bool_compare_and_swap((void **)loc, old_value, new_value)
