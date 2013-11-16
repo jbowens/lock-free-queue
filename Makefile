c7d1da127e53d5fbb55fc019c59a0f34c13fe990
Eric Caruso <eric@cs.brown.edu>
2013-11-16 14:18:49 -0500

    dubiously functional assembly

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: queue
+all: queue-32 queue-64
 
-queue:
-	gcc -std=c99 -lpthread lockfree_queue.c tester.c -o run_tests.out
+queue-32:
+	gcc -std=c99 -m32 -lpthread lockfree_queue.c tester.c atomic-i686.S -o run_tests-32.out
+
+queue-64:
+	gcc -std=c99 -m64 -lpthread lockfree_queue.c tester.c atomic-x86_64.S -o run_tests-64.out
 
 clean:
 	rm run_tests.out
--- atomic.h
+++ atomic.h
@@ -4,10 +4,23 @@
 
 typedef struct stamped_ref {
     void *ptr;
-    uint32_t stamp;
-} stamped_ref;
+    union {
+        uint32_t stamp;
+        void *_align;
+    };
+} stamped_ref_t;
 
 /**
  * Compare and set macro.
  */
 #define compare_and_set(loc, old_value, new_value) __sync_bool_compare_and_swap((void **)loc, old_value, new_value)
+
+uint32_t stamped_ref_cas(
+    stamped_ref_t *sr,
+    void *old_ref,
+    uint32_t old_stamp,
+    void *new_ref,
+    uint32_t new_stamp
+);
+
+void *stamped_ref_get(stamped_ref_t *sr, uint32_t *stamp);
