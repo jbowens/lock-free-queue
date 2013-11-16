1a7a4641081c8b9a171be3101fdec01a387bf73e
Jackson Owens <jackson_owens@brown.edu>
2013-11-16 08:08:22 -0500

    made compare and set a macro

--- atomic.h
+++ atomic.h
@@ -4,7 +4,4 @@
  * Compare and set. Intended only for 32 bit machines. Returns 1 if successful
  * 0 otherwise.
  */
-static int compare_and_set(void *loc, void *old_value, void *new_value)
-{
-    return __sync_bool_compare_and_swap((void **) loc, old_value, new_value);
-}
+#define compare_and_set(loc, old_value, new_value) __sync_bool_compare_and_swap((void **)loc, old_value, new_value)
