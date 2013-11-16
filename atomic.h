da59f85baef4cc1dfc467c71bbec9cedb10df802
Jackson Owens <jackson_owens@brown.edu>
2013-11-16 07:47:01 -0500

    removed unused macro

--- atomic.h
+++ atomic.h
@@ -1,24 +1,6 @@
 #pragma once
 
 /**
- * A raw 32-bit cmpxchg adapted from linux. Takes a lock prefix for
- * optionally memfencing before.
- */
-/*
-#define __raw_cmpxchg(ptr, old_value, new_value)                        \
-({                                                                      \
-        void *__ret;                                                    \
-        void *__old = (old_value);                                      \
-        void *__new = (new_value);                                      \
-        volatile void **__ptr = (volatile void **)(ptr);                \
-        __asm__ volatile("cmpxchgl %2,%1"                               \
-                : "=a" (__ret), "+m" (*__ptr)                           \
-                : "r" (__new), "0" (__old)                              \
-                : "memory");                                            \
-        __ret;                                                          \
-})*/
-
-/**
  * Compare and set. Intended only for 32 bit machines. Returns 1 if successful
  * 0 otherwise.
  */
