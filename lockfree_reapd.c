78e9bd8912c63206703a0c60ab478c8b5669c25a
Jackson Owens <jackson_owens@brown.edu>
2013-11-24 21:18:48 -0500

    i <3 double frees

--- lockfree_reapd.c
+++ lockfree_reapd.c
@@ -17,21 +17,39 @@ void *lockfree_reapd_main(void *arg)
     for (;;)
     {
 
-        lockfree_freenode_t *prev = attr->lfra_free_list;
-        for (lockfree_freenode_t *curr = prev->lffn_next; curr != 0; curr = curr->lffn_next)
-        {
-            /* Is the pointer to this item still in the hazard table? */
-            if (!hazard_table_search(attr->lfra_hazard_table, curr)) {
-                /* This pointer is no longer referenced. Try and unlink and free it. */
+        lockfree_freenode_t *sentinel_head = attr->lfra_free_list;
 
-                if (compare_and_set(&prev->lffn_next, curr, curr->lffn_next)) {
+        while (sentinel_head->lffn_next == 0) {
+            /* TODO: Add a sleep while the list is empty. */
+            sched_yield();
+        }
+
+        lockfree_freenode_t *gobble_head = sentinel_head->lffn_next;
+        /* Keep trying to steal the list until we're successful. */
+        while (!compare_and_set(&sentinel_head->lffn_next, gobble_head, 0)) {
+            gobble_head = sentinel_head->lffn_next;
+        }
+
+        /* gobble_head is now the head of a list of nodes that are available for
+         * freeing. We just need to free them all at our own convenience. The list
+         * will not be modified by any other thread, because we just stole it away.
+         */
+        while (gobble_head != 0) {
+            lockfree_freenode_t *prev = gobble_head;
+            for (lockfree_freenode_t *curr = gobble_head; curr != 0; curr = curr->lffn_next) {
+                if (!hazard_table_search(attr->lfra_hazard_table, curr)) {
+                    /* This node is no longer in the hazard table and it's safe to free it. */
+                    if (curr == gobble_head) {
+                        /* This is the head. Just move the head forward. */
+                        gobble_head = curr->lffn_next;
+                    } else {
+                        /* This node is within the list. Unlink it. */
+                        prev->lffn_next = curr->lffn_next;
+                    }
                     attr->lfra_free_func(curr);
                 } else {
                     prev = curr;
                 }
-            } else {
-                /* We didn't unlink curr, so move prev forward to curr. */
-                prev = curr;
             }
         }
 
